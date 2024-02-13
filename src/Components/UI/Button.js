11cebe9b4eefaafb9ed6ce1b6e9f35b5d7640e79
THT <thehelpfultipper@gmail.com>
2024-02-13 08:05:57 -0800

    Add arrow navigation for game board

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -4,7 +4,7 @@ import s from './Button.module.css';
 
 function Button({active, children, ...props}) {
     // @props.cs refers to 'custom styles' 
-    return (
+        return (
         <button
             className={`${s.Btn} ${active ? s["Btn-active"] : s["Btn-default"]}${props.cs ? ` ${props.cs}` : ''}`}
             {...props}
