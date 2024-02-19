954b17623b789b743dcf8598ecfd0ba810606d20
THT <thehelpfultipper@gmail.com>
2024-02-19 13:19:15 -0800

    Updates

--- src/Components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-
-import s from './Card.module.css';
-
-export default function Card({children, className}) {
-    return (
-        <div className={`${s["Card-default"]}${className ? ` ${className}` : ''}`}>{children}</div>
-    )
-}
